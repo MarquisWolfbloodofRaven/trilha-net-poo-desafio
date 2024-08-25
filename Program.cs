e67f3708925be9e0fca8a4f0835fce4a2ce207da
MarquisWolfbloodofRaven <raposadeserto033@gmail.com>
2024-08-25 13:06:22 -0300

    finished

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Teste com Iphone
+        Iphone iphone = new Iphone("1234567890", "iPhone 13", "012345678901234", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        // Teste com Nokia
+        Nokia nokia = new Nokia("9876543210", "Nokia 3310", "987654321098765", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake II");
+
+        Console.ReadLine();
+    }
+}
